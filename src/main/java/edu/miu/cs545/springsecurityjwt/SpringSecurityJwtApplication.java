549e19d84330a6edf01f826c6535a303f4633219
wizardofwaz8282 <104391698+wizardofwaz8282@users.noreply.github.com>
2022-07-01 13:49:14 -0500

    Testing to see if branch updated
    Took 21 minutes

--- src/main/java/edu/miu/cs545/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/edu/miu/cs545/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -1,11 +1,17 @@
 package edu.miu.cs545.springsecurityjwt;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
-
+	@Bean
+	public ModelMapper mapper(){
+		System.out.println("Here");
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+--User Insertion
+INSERT INTO users VALUES(1,'Novac','Dinosaur Lookout','0008434B','cBoone@falloutu.com','Craig','Boone','Lastthingyounever$ee');
+INSERT INTO users VALUES(2,'Freeside','Old Mormon Fort','0011ED01','aGannon@falloutu.com','Arcade I','Gannon','ForAuldLangSyne');
+
+--Category
+INSERT INTO category VALUES (1,'Stationary Object');
+INSERT INTO category VALUES (2,'Non Stationary Object');
+
+--Products
+INSERT INTO product VALUES (1,'A Bowl of rice on a counter top', 14.92, 3,1,2);
+INSERT INTO product VALUES (2,'Paper', .08, 10,2,1);
+
+--Review
+INSERT INTO review VALUES(1,'The paper is Tear-Able',1,2)
+INSERT INTO review VALUES(2,'Dont knock it till you try it, and then knock it. Three thumbs down',2,1)
